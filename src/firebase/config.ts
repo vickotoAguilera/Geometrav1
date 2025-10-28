d667154e63fd2fd81dcf9d562ea7f4e8efa5649d
victor aguilera muñoz <victoraguileramunoz@gmail.com>
2025-10-28 19:04:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1195079921-d488c",
+  "appId": "1:1055885226453:web:c6f39dc1222057e85dcd5c",
+  "apiKey": "AIzaSyCGgzq-uZ5qtZuQjdC0Zq4cX3bBGVXkZhM",
+  "authDomain": "studio-1195079921-d488c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1055885226453"
+};
