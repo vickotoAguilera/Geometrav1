28ccbb3c338cbfd19599f972e523a5b6dc1a3590
victor aguilera muñoz <victoraguileramunoz@gmail.com>
2025-10-28 23:42:09 +0000

    no esta ese proyecto talves lo tengo que enlasar a google cloud o algo a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "geogebra-476523",
+  "projectId": "studio-1195079921-d488c",
   "appId": "1:1055885226453:web:c6f39dc1222057e85dcd5c",
   "apiKey": "AIzaSyCGgzq-uZ5qtZuQjdC0Zq4cX3bBGVXkZhM",
-  "authDomain": "geogebra-476523.firebaseapp.com",
+  "authDomain": "studio-1195079921-d488c.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "1055885226453",
-  "storageBucket": "geogebra-476523.appspot.com"
+  "storageBucket": "studio-1195079921-d488c.appspot.com"
 };
