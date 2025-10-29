9f7e4271106a519434bdc16abb382cc86766d6b9
victor aguilera muñoz <victoraguileramunoz@gmail.com>
2025-10-29 00:59:52 +0000

    el mismo error yo creo que por que no tenemos storage .... por que no no

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,70 +1,74 @@
+// functions/index.js
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const Busboy = require('busboy');
-const cors = require('cors')({ origin: true });
 
 admin.initializeApp();
 
-exports.uploadFile = functions.https.onRequest((req, res) => {
-  cors(req, res, async () => {
-    if (req.method === 'OPTIONS') {
-      res.status(204).send('');
-      return;
-    }
+exports.uploadFile = functions.https.onRequest(async (req, res) => {
+  // Manejo manual de CORS
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-    if (req.method !== 'POST') {
-      return res.status(405).send('Method Not Allowed');
-    }
+  // Responder a la solicitud preflight OPTIONS
+  if (req.method === 'OPTIONS') {
+    res.status(204).send('');
+    return;
+  }
+  
+  if (req.method !== 'POST') {
+    return res.status(405).send('Method Not Allowed');
+  }
 
-    try {
-      const busboy = Busboy({ headers: req.headers });
-      let uploadData = {};
+  try {
+    const busboy = Busboy({ headers: req.headers });
+    let uploadData = {};
 
-      busboy.on('file', (fieldname, file, { filename, encoding, mimeType }) => {
-        const fileBuffer = [];
-        file.on('data', (data) => {
-          fileBuffer.push(data);
-        });
-        file.on('end', () => {
-          uploadData = {
-            buffer: Buffer.concat(fileBuffer),
-            filename,
-            mimeType,
-          };
-        });
+    busboy.on('file', (fieldname, file, { filename, encoding, mimeType }) => {
+      const fileBuffer = [];
+      file.on('data', (data) => {
+        fileBuffer.push(data);
+      });
+      file.on('end', () => {
+        uploadData = {
+          buffer: Buffer.concat(fileBuffer),
+          filename,
+          mimeType,
+        };
+      });
+    });
+
+    busboy.on('finish', async () => {
+      if (!req.query.uid) {
+         return res.status(401).send('User ID not provided');
+      }
+      
+      const { buffer, filename, mimeType } = uploadData;
+      const userId = req.query.uid;
+      
+      const bucket = admin.storage().bucket();
+      const filePath = `uploads/${userId}/${Date.now()}-${filename}`;
+      const file = bucket.file(filePath);
+
+      await file.save(buffer, {
+        metadata: {
+          contentType: mimeType,
+        },
       });
 
-      busboy.on('finish', async () => {
-        if (!req.query.uid) {
-           return res.status(401).send('User ID not provided');
-        }
-        
-        const { buffer, filename, mimeType } = uploadData;
-        const userId = req.query.uid;
-        
-        const bucket = admin.storage().bucket();
-        const filePath = `uploads/${userId}/${Date.now()}-${filename}`;
-        const file = bucket.file(filePath);
-
-        await file.save(buffer, {
-          metadata: {
-            contentType: mimeType,
-          },
-        });
-
-        const [url] = await file.getSignedUrl({
-          action: 'read',
-          expires: '03-01-2500',
-        });
-
-        res.status(200).json({ downloadURL: url });
+      const [url] = await file.getSignedUrl({
+        action: 'read',
+        expires: '03-01-2500',
       });
 
-      busboy.end(req.rawBody);
+      res.status(200).json({ downloadURL: url });
+    });
 
-    } catch (error) {
-      console.error('Upload error:', error);
-      res.status(500).json({ error: 'Upload failed' });
-    }
-  });
+    busboy.end(req.rawBody);
+
+  } catch (error) {
+    console.error('Upload error:', error);
+    res.status(500).json({ error: 'Upload failed' });
+  }
 });
